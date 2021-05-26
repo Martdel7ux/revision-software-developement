99b6ef3463dde2d3cac32ac7be391d0c2bb2b46a
Martin Ahoto <martinahoto4@gmail.com>
2021-05-27 01:05:15 +0300

    Initial Commit

---
+++ revision software developement/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  revision software developement
+//
+//  Created by MARTIN on 27/05/2021.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
